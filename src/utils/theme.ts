c4a5d747450e34e975bdefa8017f76dc6577841f
Morley Tatro <mtatro@figure.com>
2022-05-18 16:11:05 -0700

    fix the type import order

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,6 +1,6 @@
+import type { Path } from './types';
 import { lensPath, view } from 'ramda';
 export { mergeDeepRight as merge } from 'ramda';
-import type { Path } from './types';
 
 export function getThemeVal(path: Path) {
   return props =>
